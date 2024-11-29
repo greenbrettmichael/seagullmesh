1b18f0ca33ccc1916779dac82a443927679ea1f8
Darik Gamble <darik.gamble@gmail.com>
2024-11-29 12:35:16 -0500

    triangulate wip

--- src/triangulate.cpp
+++ src/triangulate.cpp
@@ -2,9 +2,12 @@
 #include "util.hpp"
 
 #include <CGAL/Polygon_mesh_processing/triangulate_hole.h>
+#include <CGAL/Polygon_mesh_processing/triangulate_faces.h>
+#include <CGAL/Polygon_mesh_processing/orientation.h>
 
 namespace PMP = CGAL::Polygon_mesh_processing;
 typedef CGAL::Triple<size_t, size_t, size_t> Triangle;
+typedef Mesh3::Property_map<V, double> VertDouble;
 
 py::array_t<size_t> constrained_contour_pair_mesh(
     const std::vector<Point3>& p,
@@ -60,8 +63,111 @@ py::array_t<size_t> constrained_contour_pair_mesh(
     return faces;
 }
 
+class TubeMesher {
+    private:
+
+    Mesh3& mesh;
+    VertDouble& t_map;
+    VertDouble& theta_map;
+    std::map<double, V> prev_xs;
+
+    std::map<double, V> _add_xs(const double t, const py::array_t<double>& theta, const py::array_t<double>& pts) {
+        const size_t n = pts.shape(0);
+        auto r_pts = pts.unchecked<2>();
+        auto r_theta = theta.unchecked<1>();
+        std::map<double, V> out;
+
+        for (size_t i = 0; i < n; ++i) {
+            Point3 pt = Point3(r_pts(i, 0), r_pts(i, 1), r_pts(i, 2));
+            V v = mesh.add_vertex(pt);
+            t_map[v] = t;
+            theta_map[v] = r_theta(i);
+            out[r_theta(i)] = v;
+        }
+        out[2 * CGAL_PI] = out[0];  // Close the loop
+        return out;
+    }
+
+    public:
+
+    TubeMesher(
+            Mesh3& mesh,
+            VertDouble& t_map,
+            VertDouble& theta_map, 
+            const double t0, 
+            const py::array_t<double>& theta0, 
+            const py::array_t<double>& pts0
+        ) : mesh(mesh), t_map(t_map), theta_map(theta_map) {
+            prev_xs = _add_xs(t0, theta0, pts0);
+    }
+    auto get_prev_xs() {
+        return prev_xs;
+    }
+    void add_xs(double t, const py::array_t<double>& theta, const py::array_t<double>& pts, size_t n_max_faces) {
+        std::map<double, V> next_xs = _add_xs(t, theta, pts);
+
+        bool debug = n_max_faces > 0;
+
+        if (debug) { std::cout << "nv  " << mesh.number_of_vertices() << "\n"; }
+
+        std::vector<V> face;
+        double theta0 = 0, theta1 = 0;
+
+        while (theta0 < 2 * CGAL_PI) {
+            // Initialize face with axial edge from prev_xs to next_xs
+            face.clear();
+            face.push_back(prev_xs[theta0]);
+            face.push_back(next_xs[theta0]);
+
+            // Walk forward on next_xs until we find a theta1 that is also present in prev_xs
+            for (auto it1 = next_xs.upper_bound(theta0); it1 != next_xs.end(); ++it1) {
+                theta1 = it1->first;
+                face.push_back(it1->second);
+
+                if (auto it0 = prev_xs.find(theta1); it0 != prev_xs.end()) {
+                    // Found matching point in prev_xs  -- Walk backwards on prev_xs until we return to theta0
+                    while (it0->first > theta0) {
+                        face.push_back(it0->second);
+                        it0--;
+                    }
+                    break;
+                }
+            }
+
+            // // 
+            // for (auto it = std::make_reverse_iterator(prev_xs.lower_bound(theta1)); it != prev_xs.rend(); ++it) {
+            //     std::cout << "what " << it->first << " " << it->secondl
+            //     if (it->first <= theta0) { break; }
+            //     face.push_back(it->second);
+            // }
+
+            // Finalize the face 
+            if (debug) {
+                std::cout << "Adding face for theta " << theta0 << " => " << theta1 << ": ";
+                for (V v : face) {
+                    std::cout << V::size_type(v) << ", "; 
+                }
+                std::cout << "\n";
+            }
+            mesh.add_face(face);
+            theta0 = theta1;
+
+            if ( debug && mesh.number_of_faces() > n_max_faces) { break; }
+        }
+        prev_xs = next_xs;
+    }
+};
+
 void init_triangulate(py::module &m) {
-    m.def_submodule("triangulate")
+    py::module sub = m.def_submodule("triangulate");
+
+    py::class_<TubeMesher>(sub, "TubeMesher")
+        .def(py::init<Mesh3&, VertDouble&, VertDouble&, double, const py::array_t<double>&, const py::array_t<double>>())
+        .def("add_xs", &TubeMesher::add_xs)
+        .def("get_prev_xs", &TubeMesher::get_prev_xs)
+    ;
+
+    sub
         .def("constrained_contour_pair_mesh", [](
             const py::array_t<double>& p_in, const py::array_t<double>& q_in,
             const std::vector<size_t>& pidx, const std::vector<size_t>& qidx, const size_t np0, const size_t nq0
@@ -70,5 +176,12 @@ void init_triangulate(py::module &m) {
             std::vector<Point3> q = array_to_points_3(q_in);
             return constrained_contour_pair_mesh(p, q, pidx, qidx, np0, nq0);
         })
+        .def("triangulate_faces", [](Mesh3& mesh, const std::vector<F>& faces) {
+            PMP::triangulate_faces(faces, mesh);
+        })
+        .def("reverse_face_orientations", [](Mesh3& mesh, const std::vector<F>& faces) {
+            PMP::reverse_face_orientations(faces, mesh);
+        })
+
     ;
 }
