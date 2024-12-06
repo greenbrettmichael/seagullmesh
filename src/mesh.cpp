0367525a6ad540e8a4dba75ba2b415e0bca14711
Darik Gamble <darik.gamble@gmail.com>
2024-12-05 19:23:04 -0500

    add a transform method

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -15,6 +15,32 @@
 namespace PMP = CGAL::Polygon_mesh_processing;
 
 typedef CGAL::Bbox_3 BBox3;
+typedef CGAL::Aff_transformation_3<Kernel> Transform3;
+
+
+Transform3 array_to_transform3(const py::array_t<double>& transform) {
+    auto r = transform.unchecked<2>();
+    if (r.shape(0) != r.shape(1)) {
+        throw py::value_error("Must be a square matrix");
+    }
+    if (r.shape(0) == 3) {
+        return Transform3(
+            r(0, 0), r(0, 1), r(0, 2),
+            r(1, 0), r(1, 1), r(1, 2),
+            r(2, 0), r(2, 1), r(2, 2)
+        );
+    } else if (r.shape(0) == 4) {
+        return Transform3(
+            r(0, 0), r(0, 1), r(0, 2), r(0, 3),
+            r(1, 0), r(1, 1), r(1, 2), r(1, 3),
+            r(2, 0), r(2, 1), r(2, 2), r(2, 3),
+                                       r(3, 3)
+        );
+    } else {
+        throw py::value_error("Must be a 3x3 or 4x4 matrix");
+    }
+}
+
 
 template<typename T>
 void define_simple_type_3(py::module &m, std::string name) {
@@ -215,6 +241,7 @@ void init_mesh(py::module &m) {
 
     py::class_<Mesh3>(sub, "Mesh3")
         .def(py::init<>())
+        .def(py::init<const Mesh3&>())
 
         .def_property_readonly("has_garbage", [](const Mesh3& mesh) {return mesh.has_garbage();})
         .def("collect_garbage", [](Mesh3& mesh) {mesh.collect_garbage();})
@@ -246,6 +273,13 @@ void init_mesh(py::module &m) {
 
         .def("bounding_box", [](const Mesh3& mesh) {
             return PMP::bbox(mesh);
+        })
+        .def("transform", [](Mesh3& mesh, const py::array_t<double>& transform) {
+            Transform3 t = array_to_transform3(transform);
+            auto points = mesh.points();
+            for (V v : mesh.vertices() ) {
+                points[v] = t(points[v]);
+            }
         })
 
         .def("edge_vertices", [](const Mesh3& mesh, const std::vector<E>& edges) {
