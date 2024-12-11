cc1eb951142fb6dfb7558c830934275a5eb8a51e
Darik Gamble <darik.gamble@gmail.com>
2024-12-11 16:18:44 -0500

    hmm

--- src/corefine.cpp
+++ src/corefine.cpp
@@ -4,57 +4,53 @@
 
 namespace PMP = CGAL::Polygon_mesh_processing;
 
-//typedef std::pair<uint32_t, V>                                      VertexOrigin;
-//typedef CGAL::dynamic_vertex_property_t<VertexOrigin>               VertexOriginProperty;
-//typedef boost::property_map<Mesh3, VertexOriginProperty>::type      VertexOriginPMap;
-
-typedef std::pair<size_t, F>                                        FaceOrigin;
-typedef CGAL::dynamic_face_property_t<FaceOrigin>                   FaceOriginProperty;
-typedef boost::property_map<Mesh3, FaceOriginProperty>::type        FaceOriginPropertyMap;
-
-typedef CGAL::dynamic_face_property_t<F>                            FaceId;
-typedef boost::property_map<Mesh3, FaceId>::type                    FaceIdPropertyMap;
-
-
-// todo: need pmap[F, F] on mesh1 and mesh2
 struct CorefineTracker : public PMP::Corefinement::Default_visitor<Mesh3> {
+    typedef std::map<F, F> FaceMap;
+    struct FaceOrigin {
+        mesh_idx: size_t
+        face: F
+    };
     Mesh3& mesh1;
     Mesh3& mesh2;
-    Mesh3& output;
-    std::array<FaceIdPropertyMap, 2> original_faces;
 
-    FaceOrigin face_origin;
-    FaceOriginPropertyMap face_origins;
+    std::array<FaceMap, 2> split_faces;     // subface -> original face for each source mesh
+    std::array<FaceMap, 2> copied_faces;    // subface in output -> orig_face
 
-    CorefineTracker(Mesh3& m1, Mesh3& m2, Mesh3& out) : mesh1(m1), mesh2(m2), output(out) {
-        original_faces = {
-            get(FaceId(), mesh1, Mesh3::null_face()),
-            get(FaceId(), mesh2, Mesh3::null_face())
-        };
-        for (F f : mesh1.faces()) { put(original_faces[0], f, f); }
-        for (F f : mesh2.faces()) { put(original_faces[1], f, f); }
+    CorefineTracker(Mesh3& m1, Mesh3& m2) : mesh1(m1), mesh2(m2) {};
 
-        face_origin = std::make_pair(2, Mesh3::null_face());
-        face_origins = get(FaceOriginProperty(), output, face_origin);
-    }
-    uint32_t mesh_idx(const Mesh3& mesh) {
+    uint32_t mesh_idx(const Mesh3& mesh) const {
         if (&mesh == &mesh1) {return 0;} else if (&mesh == &mesh2) {return 1;} else {return 2;};
     }
 
-    void before_subface_creations(F f_to_be_split, Mesh3& mesh) {
+    F original_face(size_t mesh_idx, F face) const {
+        auto it = split_faces[i].find(face);
+        return (it != split_faces[i].end()) ? it->second : face;
+    }
+
+    void before_subface_creations(F f, const Mesh3& mesh) {
         // Going to split a face in mesh1 or mesh2 during the corefinement stage
         auto i = mesh_idx(mesh);
-        F f_original = get(original_faces[i], f_to_be_split);
-        face_origin = std::make_pair(i, f_original);
+        face_origin = {i, original_face(i, f)};
     }
     void after_subface_created(F f_new, const Mesh3& mesh) {
-        auto i = face_origin.first;
-        F f_original = face_origin.second;
-        put(original_faces[i], f_new, f_original);
+        split_faces[face_origin.mesh_idx][f_new] = face_origin.face;
     }
     void after_face_copy(F f_src, const Mesh3& m_src, F f_tgt, const Mesh3& m_tgt) {
-        put(face_origins, f_tgt, std::make_pair(mesh_idx(m_src), f_tgt));
+        auto i = mesh_idx(m_src);
+        copied_faces[i][f_tgt] = original_face(i, f_src);
     }
+
+//    void get_mapped_faces(size_t i) {
+//        std::vector<F> faces_input;
+//        std::vector<F> faces_output;
+//        for (F f : output.faces()) {
+//            const FaceOrigin& fo = get(face_origins, f);
+//            if (fo.first == i) {
+//                faces_input.push_back(fo.second);
+//                faces_output.
+//            }
+//        }
+//    }
 };
 
 
@@ -66,7 +62,7 @@ void init_corefine(py::module &m) {
     py::module sub = m.def_submodule("corefine");
 
     py::class_<CorefineTracker>(sub, "CorefineTracker")
-        .def(py::init<Mesh3&, Mesh3&, Mesh3&>())
+        .def(py::init<Mesh3&, Mesh3&>())
     ;
 
 
