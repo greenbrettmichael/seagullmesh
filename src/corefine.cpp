91f845ad32b9e1e76e8384af801867d2e86fa331
Darik Gamble <darik.gamble@gmail.com>
2024-12-11 14:33:06 -0500

    hmm

--- src/corefine.cpp
+++ src/corefine.cpp
@@ -8,9 +8,12 @@ namespace PMP = CGAL::Polygon_mesh_processing;
 //typedef CGAL::dynamic_vertex_property_t<VertexOrigin>               VertexOriginProperty;
 //typedef boost::property_map<Mesh3, VertexOriginProperty>::type      VertexOriginPMap;
 
-typedef std::pair<uint32_t, F>                                      FaceOrigin;
+typedef std::pair<size_t, F>                                        FaceOrigin;
 typedef CGAL::dynamic_face_property_t<FaceOrigin>                   FaceOriginProperty;
 typedef boost::property_map<Mesh3, FaceOriginProperty>::type        FaceOriginPropertyMap;
+
+typedef CGAL::dynamic_face_property_t<F>                            FaceId;
+typedef boost::property_map<Mesh3, FaceId>::type                    FaceIdPropertyMap;
 
 
 // todo: need pmap[F, F] on mesh1 and mesh2
@@ -18,32 +21,39 @@ struct CorefineTracker : public PMP::Corefinement::Default_visitor<Mesh3> {
     Mesh3& mesh1;
     Mesh3& mesh2;
     Mesh3& output;
+    std::array<FaceIdPropertyMap, 2> original_faces;
+
     FaceOrigin face_origin;
     FaceOriginPropertyMap face_origins;
 
-
     CorefineTracker(Mesh3& m1, Mesh3& m2, Mesh3& out) : mesh1(m1), mesh2(m2), output(out) {
+        original_faces = {
+            get(FaceId(), mesh1, Mesh3::null_face()),
+            get(FaceId(), mesh2, Mesh3::null_face())
+        }
+        for (F f : mesh1.faces()) { put(original_faces[0], f, f); }
+        for (F f : mesh2.faces()) { put(original_faces[1], f, f); }
+
         face_origin = std::make_pair(2, Mesh3::null_face());
         face_origins = get(FaceOriginProperty(), output, face_origin);
     }
     uint32_t mesh_idx(const Mesh3& mesh) {
         if (&mesh == &mesh1) {return 0;} else if (&mesh == &mesh2) {return 1;} else {return 2;};
     }
-//    void after_vertex_copy(V v_src, const Mesh3& m_src, V v_tgt, const Mesh3& m_tgt) {
-//        vertex_origin[v_tgt] = std::make_pair(mesh_idx(m_src), v_src)
-//    }
-//    void new_vertex_added(size_t i_id, V v, const Mesh3& mesh) {
-//        vert_mesh_idx[v] = 2;
-//    }
 
-    void after_face_copy(F f_src, const Mesh3& m_src, F f_tgt, const Mesh3& m_tgt) {
-        put(face_origins, f_tgt, std::make_pair(mesh_idx(m_src), f_tgt));
-    }
-    void before_subface_creations(F f, Mesh3& mesh) {
-        face_origin = std::make_pair(mesh_idx(mesh), f);
+    void before_subface_creations(F f_to_be_split, Mesh3& mesh) {
+        // Going to split a face in mesh1 or mesh2 during the corefinement stage
+        auto i = mesh_idx(mesh);
+        F f_original = original_faces[i][f_to_be_split]
+        face_origin = std::make_pair(i, f_original);
     }
     void after_subface_created(F f_new, const Mesh3& mesh) {
-        put(face_origins, f_new, face_origin);
+        auto i = face_origin.first;
+        F f_original = face_origin.second;
+        put(original_faces[i], f_new, f_original);
+    }
+    void after_face_copy(F f_src, const Mesh3& m_src, F f_tgt, const Mesh3& m_tgt) {
+        put(face_origins, f_tgt, std::make_pair(mesh_idx(m_src), f_tgt));
     }
 };
 
